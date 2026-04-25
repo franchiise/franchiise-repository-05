36a751d7538d5a2b762b987a6a81a799d375df2e
Francisco Biasoli <72677686+franchiise@users.noreply.github.com>
2026-04-25 17:49:05 -0300

    Add files via upload

---
+++ Funcoes/src/funcoes/Funcoes.java
@@ -0,0 +1,15 @@
+package funcoes;
+
+public class Funcoes {
+
+    public static void main(String[] args) {
+
+        NewClassFatorialFuncao f = new NewClassFatorialFuncao();
+        f.setValor(9);
+        
+        System.out.print(f.getFormula());
+        System.out.println(f.getFatorial());
+        
+    }
+    
+}
